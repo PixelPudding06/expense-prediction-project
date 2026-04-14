a4f8e2215e0f145b3b0c462a1a738c8bf1256222
PixelPudding06 <kr5877771@gmail.com>
2026-04-14 16:26:44 +0530

    Stable colorful dashboard version

--- budgetadvisor/urls.py
+++ budgetadvisor/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from expenses.views import home
+from expenses.views import dashboard, add_expense, history, delete_expense, chart, edit_expense
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', dashboard),
+    path('add/', add_expense),
+    path('history/', history),
+    path('chart/', chart),
+    path('edit/<int:id>/', edit_expense),
+    path('delete/<int:id>/', delete_expense),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,27 +1,87 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Expense
+from sklearn.linear_model import LinearRegression
 
-def home(request):
-    data = Expense.objects.all()
+def dashboard(request):
+    data = Expense.objects.all().order_by('-date')
 
-    total = 0
-    count = 0
+    total = sum(x.amount for x in data)
 
-    for x in data:
-        total += x.amount
-        count += 1
+    amounts = [x.amount for x in data]
+    months = [[i+1] for i in range(len(amounts))]
 
-    if count > 0:
-        prediction = total + 500
+    if len(amounts) > 1:
+        model = LinearRegression()
+        model.fit(months, amounts)
+        prediction = int(model.predict([[len(amounts)+1]])[0])
+
+    elif len(amounts) == 1:
+        prediction = amounts[0]
+
     else:
         prediction = 0
 
-    budget = 5000
-    remaining = budget - total
+    remaining = 5000 - total
 
-    return render(request, 'home.html', {
-        'data': data,
-        'prediction': prediction,
+    labels = []
+    values = []
+
+    for x in data:
+        labels.append(x.category)
+        values.append(x.amount)
+
+    return render(request, 'dashboard.html', {
+        'data': data[:5],
         'total': total,
-        'remaining': remaining
+        'prediction': prediction,
+        'remaining': remaining,
+        'labels': labels,
+        'values': values
     })
+
+def add_expense(request):
+    if request.method == "POST":
+        Expense.objects.create(
+            category=request.POST['category'],
+            amount=request.POST['amount'],
+            date=request.POST['date']
+        )
+        return redirect('/history/')
+
+    return render(request, 'add_expense.html')
+
+
+def history(request):
+    data = Expense.objects.all().order_by('-date')
+    return render(request, 'history.html', {'data': data})
+
+
+def delete_expense(request, id):
+    item = get_object_or_404(Expense, id=id)
+    item.delete()
+    return redirect('/history/')
+def chart(request):
+    data = Expense.objects.all()
+
+    labels = []
+    values = []
+
+    for x in data:
+        labels.append(x.category)
+        values.append(x.amount)
+
+    return render(request, 'chart.html', {
+        'labels': labels,
+        'values': values
+    })
+def edit_expense(request, id):
+    item = get_object_or_404(Expense, id=id)
+
+    if request.method == "POST":
+        item.category = request.POST['category']
+        item.amount = request.POST['amount']
+        item.date = request.POST['date']
+        item.save()
+        return redirect('/history/')
+
+    return render(request, 'edit.html', {'item': item})
